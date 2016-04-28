1dda90c761d9d751bc0b0a510df20e07c9208b96
ZerOne10 <michi.mueller1709@gmail.com>
2016-04-28 16:08:15 +0200

    Update sharedmemory.h

---
+++ sharedmemory.h
@@ -0,0 +1,46 @@
+/**
+ * @file sharedmemory.h
+ * sharedmemory
+ * Beispiel 3
+ *
+ * @author Karin Kalman <karin.kalman@technikum-wien.at>
+ * @author Michael Mueller <michael.mueller@technikum-wien.at>
+ * @author Gerhard Sabeditsch <gerhard.sabeditsch@technikum-wien.at>
+ * @date 2016/04/27
+ *
+ * @version $Revision: 1 $
+ *
+ *
+ * URL: $HeadURL$
+ *
+ * Last Modified: $Author: Gerhard $
+ */
+
+
+#ifndef _SHAREDMEMORY_H_
+#define _SHAREDMEMORY_H_
+
+/*
+ * -------------------------------------------------------------- includes --
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+/*
+ * --------------------------------------------------------------- defines --
+ */
+
+
+
+/*
+ * --------------------------------------------------- function prototypes --
+ */
+
+
+
+#endif /* _SHAREDMEMORY_H_ */
