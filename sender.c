1dba1c3a8766763d74094dbc847613e71de89d72
ZerOne10 <michi.mueller1709@gmail.com>
2016-04-28 16:08:00 +0200

    Update sender.c

---
+++ sender.c
@@ -0,0 +1,39 @@
+/**
+ * @file sender.c
+ * sharedmemory
+ * Beispiel 3
+ *
+ * @author Karin Kalman <karin.kalman@technikum-wien.at>
+ * @author Michael Mueller <michael.mueller@technikum-wien.at>
+ * @author Gerhard Sabeditsch <gerhard.sabeditsch@technikum-wien.at>
+ * @date 2016/04/17
+ *
+ * @version $Revision: 1 $
+ *
+ *
+ * URL: $HeadURL$
+ *
+ * Last Modified: $Author: Gerhard $
+ */
+
+/**
+ * -------------------------------------------------------------- includes --
+ */
+#include "sharedmemory.h"
+
+/**
+ * -------------------------------------------------------------- global static variables --
+ */
+
+ /*
+ * ------------------------------------------------------------- main --
+ */
+int main(int argc, const char * argv[]) {
+
+	// 1) Methode um Shared Memory anzulegen aufrufen
+	// 2) Methode um Semaphoren anzulegen aufrufen
+	// 3) while (!= EOF) auf Shared Memory schreiben
+	//		aufpassen das Schreibindex hinter Leseindex bleibt
+
+	return 0;
+}
