7c9c52536de7017f279a77e2f7721fb0b9b2eb00
ZerOne10 <michi.mueller1709@gmail.com>
2016-04-28 16:07:05 +0200

    Update empfaenger.c

---
+++ empfaenger.c
@@ -0,0 +1,40 @@
+/**
+ * @file empfaenger.c
+ * sharedmemory
+ * Beispiel 3
+ *
+ * @author Karin Kalman <karin.kalman@technikum-wien.at>
+ * @author Michael Mueller <michael.mueller@technikum-wien.at>
+ * @author Gerhard Sabeditsch <gerhard.sabeditsch@technikum-wien.at>
+ * @date 2016/04/17
+ *
+ * @version $Revision: 1 $
+ *
+ *
+ * URL: $HeadURL$
+ *
+ * Last Modified: $Author: Gerhard $
+ */
+
+/**
+ * -------------------------------------------------------------- includes --
+ */
+#include "sharedmemory.h"
+
+/**
+ * -------------------------------------------------------------- global static variables --
+ */
+
+ /*
+ * ------------------------------------------------------------- main --
+ */
+int main(int argc, const char * argv[]) {
+
+	// 1) Methode um Shared Memory anzulegen aufrufen
+	// 2) Methode um Semaphoren anzulegen aufrufen
+	// 3) while (!= EOF) von Shared Memory lesen
+	//		aufpassen das Leseindex hinter Schreibindex bleibt
+
+	return 0;
+}
+
