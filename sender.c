9ed30772693b4d528c233b19d6d559f091058637
ZerOne10 <michi.mueller1709@gmail.com>
2016-05-18 16:49:06 +0200

    Update sender.c

--- sender.c
+++ sender.c
@@ -42,6 +42,7 @@ int main(int argc, char * argv[]) {
 	// 3) while (!= EOF) auf Shared Memory schreiben
 	//		aufpassen das Schreibindex hinter Leseindex bleibt
 	do {
+		/* Decrement write semaphore, because we write onto a free segment, if >0 all ok else block application */
 		if (P(shm_sem.sem_w) != 0) {
 			if (errno == EINTR) {
 				/* syscall interrupted by signal, try again */
@@ -52,14 +53,17 @@ int main(int argc, char * argv[]) {
 			break;
 		}
 	  
+		/* get character from stdin and write into shared memory */
 		input = fgetc(stdin);
 		shm_sem.segment[pos] = input;
 		pos++;
 		
+		/* if pos == shared memory size, start at 0 again */
 		if (pos == shm_sem.shm_size) {
 			pos = 0;
 		}
 		
+		/* increment read semaphore to tell receiver that something is ready to read */
 		if (V(shm_sem.sem_r) != 0) {
 			perror("V(shm_sem.sem_r)");
 			closeSegment(shm_sem);
